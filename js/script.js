b620aba8e82dcdc9b67b2a74a0ace305a27a093e
jiaengoh <hihihi_97@hotmail.com>
2020-09-21 15:35:07 +0800

    UPDATE
    UPDATE

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$( ".inner-switch" ).on("click", function() {
+    if( $( "body" ).hasClass( "dark" )) {
+      $( "body" ).removeClass( "dark" );
+      $( ".inner-switch" ).text( "OFF" );
+    } else {
+      $( "body" ).addClass( "dark" );
+      $( ".inner-switch" ).text( "ON" );
+    }
+});
+
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+
